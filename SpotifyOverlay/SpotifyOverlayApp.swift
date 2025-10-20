2aea250432ebbe8bcbf3b60e9b72ed48f92b989e
Alex Ruddell <imalexruddell@gmail.com>
2025-10-20 16:46:57 +1300

    Initial Commit

---
+++ SpotifyOverlay/SpotifyOverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyOverlayApp.swift
+//  SpotifyOverlay
+//
+//  Created by Alex Ruddell on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyOverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
