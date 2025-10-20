f90712294605d7d686e3e0f7eba22179d08e0f07
Alex Ruddell <imalexruddell@gmail.com>
2025-10-20 20:51:14 +1300

    Attempt to load images from CDN

--- SpotifyOverlay/View/ContentView.swift
+++ SpotifyOverlay/View/ContentView.swift
@@ -6,14 +6,22 @@
 //
 
 import SwiftUI
+import SDWebImage
+import SDWebImageSwiftUI
 
 struct ContentView: View {
+    let imageUrl = "https://i.scdn.co/image/ab67616d0000b273c58b581c98eafab982391a6d.jpg"
+    
     var body: some View {
         VStack {
-            
             Text("Connect to Spotify!")
             Text("Insert album art here")
             
+            WebImage(url:URL(string: imageUrl))
+                .resizable()
+                .indicator(.activity)
+                .scaledToFit()
+                .frame(width: 100.0, height:100.0)
 
         }
         .padding()
